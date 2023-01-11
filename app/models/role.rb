be4559755f676e1021e412cbe65fe8cfede6ca2f
sandym <91996681+sandyml@users.noreply.github.com>
2023-01-11 13:57:04 -0500

    Progress

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,20 +3,30 @@ class Role < ActiveRecord::Base
 #  has_many :auditions, dependent: :destroy
 # `Role#auditions` returns all of the auditions associated with this role 
 
+# `Role#actors` returns an array of names from the actors associated with this role
+# pry(main)> Role.first.actors
+# pry(#<Role>)> self.auditions.map {|audition| audition.actor}
  def actors 
     #  auditions.pluck(:actor)
-    self.auditions.map do |audition|
-        audition.actor
-    end
+   #  self.auditions.map do |audition|
+   #      audition.actor
+   #  end
+   self.auditions.map {|audition| audition.actor}
  end
 
+# `Role#locations` returns an array of locations from the auditions associated with this role
+#  Role.first.locations
+#  pry(#<Role>)> self.auditions.map {|audition| audition.location}
  def locations
     #  auditions.pluck(:location)
-    self.auditions.map do |audition|
-        audition.location
-    end
+   #  self.auditions.map do |audition|
+   #      audition.location
+   #  end
+   self.auditions.map {|audition| audition.location}
+   # binding.pry
  end
 
+# `Role#lead` returns the first instance of the audition that was hired for this role or returns a string 'no actor has been hired for this role'
  def lead
     #  if auditions.where(hired: true).length > 0
     #      auditions.where(hired: true)[0]
@@ -33,6 +43,5 @@ class Role < ActiveRecord::Base
     #      "no actor has been hired for understudy for this role"
     #  end
     self.auditions.where(hired: true).second ||  'no actor has been hired for understudy for this role'
-
  end
 end
